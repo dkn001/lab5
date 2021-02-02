9b5c34d54e29ea0cc4eedce4c7e0e11ed8318c51
Dang K Nguyen <dkn001@ucsd.edu>
2021-02-02 12:00:21 -0800

    node.js

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,11 +10,17 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+
+	$("a").click(function(e) {
+		e.preventDefault();
+		$(this).text(anagrammedName($(this).text()));
+	});
+
 }
 
 function anagrammedName(name) {
 	// Thanks, Internet Anagram Server!
-	
+
 	if (name == "Doug Engelbart") {
 		return "Notable Grudge";
 	} 
