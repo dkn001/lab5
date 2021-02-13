ffa8e7fa3ff24cd4cbe05de6b5c192d9a586d109
Dang K Nguyen <dkn001@ucsd.edu>
2021-02-12 20:46:39 -0800

    A5

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var handlebars = require('express3-handlebars');
 var index = require('./routes/index');
 var add = require('./routes/add');
 
+//lab6
+var project = require('./routes/project');
+
 // Example route
 // var user = require('./routes/user');
 
@@ -39,6 +42,10 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/add', add.addFriend);
+
+
+//from lab6
+app.get('/project/:id', project.projectInfo);
 
 // Example route
 // app.get('/users', user.list);
--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';*/
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
@@ -11,11 +11,17 @@ $(document).ready(function() {
 function initializePage() {
 	console.log("Javascript connected!");
 
+/*
 	$("a").click(function(e) {
 		e.preventDefault();
 		$(this).text(anagrammedName($(this).text()));
 	});
+*/
 
+	//from lab6
+	$('.project a').click(addProjectDetails);
+
+	// $('#colorBtn').click(randomizeColors);
 }
 
 function anagrammedName(name) {
@@ -49,4 +55,34 @@ function anagrammedName(name) {
 		console.log(name + " not known for anagramming.");
 		return name;
 	}
+}
+
+
+
+
+
+//from lab 6
+
+/*
+ * Make an AJAX call to retrieve project details and add it in
+ */
+function addProjectDetails(e) {
+	// Prevent following the link
+	e.preventDefault();
+
+	// Get the div ID, e.g., "project3"
+	var projectID = $(this).closest('.project').attr('id');
+	// get rid of 'project' from the front of the id 'project3'
+	var idNumber = projectID.substr('project'.length);
+
+	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/"+idNumber, callBackFn);
+
+}
+
+function callBackFn(result) {
+	console.log(result);
+
+	$("#project"+result['id']+" .details").html("<p>"+result['title']+"</p>"  +   "<p>"+result['date']+"</p>"   +  "<img class='detailsImage' src='" + result['image'] + "' >"   +result['summary']);
 }
