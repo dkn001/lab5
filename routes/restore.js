743de8085e546ba5fac2698e7fed8a63c0d7d852
Dang K Nguyen <dkn001@ucsd.edu>
2021-03-05 10:49:16 -0800

    update1

--- routes/restore.js
+++ routes/restore.js
@@ -4,15 +4,13 @@ exports.restoreTask = function(request, response) { 
   // restore the note by adding it back
 
 
-	var notename = request.params.notename;
-//console.log(notename)
-	var task = data.trash.find(o => o.name === notename);
-//console.log(task)
-	data.trash = data.trash.filter(function(el) { return el.name != notename; }); 
+  var task = data.trash[data.trash.length - 1];
 
+  data.trash.pop();
 
   data.otherdatenotes.push(task);
   data.notes.push(task);
 
   response.render('trash',data);
+
  };
